3f05a66a5a6171c57e9899d0c6272eb156cb8d25
rocketnia <rokitna@hotmail.com>
2021-08-14 14:14:02 -0700

    Instead of passing just a type to Fexpress's eval and apply, pass in a structured continuation.

--- fexpress-lib/proof-of-concept.rkt
+++ fexpress-lib/proof-of-concept.rkt
@@ -26,7 +26,7 @@
 ; TODO: Figure out a better way to make this conditional than
 ; commenting it out.
 ;(require (only-in racket/pretty pretty-print))
-(require (only-in racket/generic define-generics))
+(require (only-in racket/generic define/generic define-generics))
 (require (only-in racket/hash hash-union))
 (require (only-in racket/syntax format-symbol))
 
@@ -42,86 +42,76 @@
 (define-namespace-anchor here)
 
 
-(define-generics typed
-  (typed-get-type typed))
-
 (define-generics fexpr
   (fexpr-macroexpand macro-env local-env op-var fexpr args)
-  (fexpr-apply-for-type-first env type fexpr args))
+  (fexpr-apply env cont fexpr args))
 
-(struct any-value/t () #:transparent)
+(define-generics continuation-expr
+  (continuation-expr-continue-eval continuation-expr val))
 
-(struct fx-given/t (env type args) #:transparent)
+; Positive types
+(define-generics type+
+  (type+-continue-eval type+ val))
 
-(struct type-first (type get-value) #:transparent)
+(struct any-value/t+ () #:transparent
+  #:methods gen:type+
+  [(define (type+-continue-eval type+ val)
+     val)])
 
-(struct makeshift-fexpr (macroexpand apply-for-type-first)
+(struct continuation-expr-done (type+)
   #:transparent
+  #:methods gen:continuation-expr
+  [(define (continuation-expr-continue-eval cont val)
+     (match-define (continuation-expr-done type+) cont)
+     (type+-continue-eval type+ val))])
+
+(struct continuation-expr-apply (env args next)
+  #:transparent
+  #:methods gen:continuation-expr
+  [(define/generic continuation-expr-continue-eval-
+     continuation-expr-continue-eval)
+   (define (continuation-expr-continue-eval cont val)
+     (match-define (continuation-expr-apply apply-env args next) cont)
+     (continuation-expr-continue-eval- next
+       (fexpress-apply apply-env next val args)))])
+
+(struct makeshift-fexpr (macroexpand apply) #:transparent
   #:methods gen:fexpr
   [(define (fexpr-macroexpand macro-env local-env op-var op-val args)
-     (match-define (makeshift-fexpr macroexpand apply-for-type-first)
-       op-val)
+     (match-define (makeshift-fexpr macroexpand apply) op-val)
      (macroexpand macro-env local-env op-var args))
-   (define (fexpr-apply-for-type-first env type op args)
-     (match-define (makeshift-fexpr macroexpand apply-for-type-first)
-       op)
-     (apply-for-type-first env type args))])
-
-(define (get-type v)
-  (if (typed? v)
-    (typed-get-type v)
-    (any-value/t)))
-
-(define (type-not-actually-first v)
-  (type-first (get-type v) (lambda () v)))
+   (define (fexpr-apply env cont op args)
+     (match-define (makeshift-fexpr macroexpand apply) op)
+     (apply env cont args))])
 
 (define (literal? v)
   (exact-integer? v))
 
-(define (fexpress-eval env type expr)
-  (unify-type-with-type-of-value type
-    (match expr
-      [`(,op-expr . ,args)
-       (define op-val
-         (fexpress-eval env (fx-given/t env type args) op-expr))
-       (fexpress-apply env type op-val args)]
-      [(? symbol? var) (environment-get env var)]
-      [(? literal? val) val]
-      [_ (error "Unrecognized expression")])))
+(define (fexpress-eval env cont expr)
+  (match expr
+    [`(,op-expr . ,args)
+     (fexpress-eval
+       env (continuation-expr-apply env args cont) op-expr)]
+    [(? symbol? var)
+     (continuation-expr-continue-eval cont (environment-get env var))]
+    [(? literal? val) (continuation-expr-continue-eval cont val)]
+    [_ (error "Unrecognized expression")]))
 
 (define (environment-get env var)
   (hash-ref env var (lambda () (error "Unbound variable"))))
 
-(define (fexpress-apply-for-type-first env type op args)
+(define (fexpress-apply env cont op args)
   (cond
-    [(fexpr? op) (fexpr-apply-for-type-first env type op args)]
+    [(fexpr? op) (fexpr-apply env cont op args)]
     [(procedure? op)
      (unless (and (list? args)
                   (procedure-arity-includes? op (length args)))
        (error "Wrong number of arguments to a procedure"))
-     (type-not-actually-first
-       (unify-type-with-type-of-value type
-         (apply op
-           (for/list ([arg (in-list args)])
-             (fexpress-eval env (any-value/t) arg)))))]
+     (apply op
+       (for/list ([arg (in-list args)])
+         (fexpress-eval env (continuation-expr-done (any-value/t+))
+                        arg)))]
     [#t (error "Uncallable value")]))
-
-(define (fexpress-apply env type op args)
-  (match-define (type-first actual-type get-value)
-    (fexpress-apply-for-type-first env type op args))
-  (get-value))
-
-(define (unify-type-with-type-of-value type val)
-  (match type
-    [(any-value/t) val]
-    [(fx-given/t env type args)
-     ; NOTE: We call this just for the errors.
-     ; TODO: Figure out if we even should do this. As long as we can
-     ; propagate types to the arguments of lambdas so that we can tell
-     ; when they're not fexprs, we should be good to go. Hmm, maybe we
-     ; don't even really need the types.
-;     (fexpress-apply-for-type-first env type val args)
-     val]))
 
 (struct compilation-result (depends-on-env? free-vars expr))
 
@@ -130,7 +120,9 @@
 
 (define (fexpress-fail-to-compile expr)
   (compilation-result #t (hash)
-    `(,#'fexpress-eval env (,#'any-value/t) ',expr)))
+    `(,#'fexpress-eval env
+                       (,#'continuation-expr-done (,#'any-value/t+))
+                       ',expr)))
 
 (define (fexpress-macroexpand macro-env local-env op-var op-val args)
   (cond
@@ -172,8 +164,12 @@
         (match-define (compilation-result _ free-vars op-compiled)
           (fexpress-compile macro-env local-env op-expr))
         (compilation-result #t free-vars
+          ; TODO: We probably want to pass a better continuation here.
           `(,#'fexpress-apply
-             env (,#'any-value/t) ,op-compiled ',args))]
+             env
+             (,#'continuation-expr-done (,#'any-value/t+))
+             ,op-compiled
+             ',args))]
        [(hash-has-key? local-env op-expr)
         (fexpress-fail-to-compile expr)]
        [#t
@@ -218,20 +214,20 @@
     #;compile
     (lambda (macro-env local-env op-var args)
       #f)
-    #;apply-for-type-first
-    (lambda (env type args)
+    #;apply
+    (lambda (env cont args)
       (match-define (parsed-lambda-args n arg-vars body)
         (parse-lambda-args args))
-      (type-not-actually-first
-        (lambda arg-values
-          (unless (equal? n (length arg-values))
-            (error "ilambda: wrong number of arguments at call time"))
-          (define local-env
-            (for/fold ([env env])
-                      ([arg-var (in-list arg-vars)]
-                       [arg-value (in-list arg-values)])
-              (hash-set env arg-var arg-value)))
-          (fexpress-eval local-env (any-value/t) body))))))
+      (lambda arg-values
+        (unless (equal? n (length arg-values))
+          (error "ilambda: wrong number of arguments at call time"))
+        (define local-env
+          (for/fold ([env env])
+                    ([arg-var (in-list arg-vars)]
+                     [arg-value (in-list arg-values)])
+            (hash-set env arg-var arg-value)))
+        (fexpress-eval
+          local-env (continuation-expr-done (any-value/t+)) body)))))
 
 (define (compile-clambda macro-env local-env args)
   (match-define (parsed-lambda-args n arg-vars body)
@@ -272,8 +268,8 @@
     #;macroexpand
     (lambda (macro-env local-env op-var args)
       (compile-clambda macro-env local-env args))
-    #;apply-for-type-first
-    (lambda (env type args)
+    #;apply
+    (lambda (env cont args)
       (match-define
         (compilation-result depends-on-env? free-vars lambda-compiled)
         (compile-clambda env (hash) args))
@@ -292,14 +288,14 @@
       (define lambda-maker
         (eval lambda-maker-compiled
               (namespace-anchor->namespace here)))
-      (type-not-actually-first
-        (apply lambda-maker env
-          (for/list ([free-var (in-list free-vars-list)])
-            (environment-get env free-var)))))))
+      (apply lambda-maker env
+        (for/list ([free-var (in-list free-vars-list)])
+          (environment-get env free-var))))))
 
 (define (fexpress-make-base-env)
   (hash 'eval fexpress-eval
-        'any-value/t any-value/t
+        'continuation-expr-done continuation-expr-done
+        'any-value/t+ any-value/t+
         'make-base-env fexpress-make-base-env
         'ilambda fexpress-ilambda
         'clambda fexpress-clambda
@@ -308,4 +304,6 @@
         'app (lambda (op . args) (apply op args))))
 
 (define (fexpress-eval-in-base-env expr)
-  (fexpress-eval (fexpress-make-base-env) (any-value/t) expr))
+  (fexpress-eval (fexpress-make-base-env)
+                 (continuation-expr-done (any-value/t+))
+                 expr))
